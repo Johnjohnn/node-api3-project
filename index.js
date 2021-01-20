59f3b78f15cfe72992a412529c376259d55e0108
johnjohnn <conde2010@aol.com>
2021-01-19 20:47:47 -0500

    adding middleware

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code away!
+const express = require ("express")
+const userRouter = require("./users/userRouter")
+
+
+
+
+const server =  express()
+
+const port = 4000 
+// takes incomingrequest JSON data and parses it into req.body
+server.use(express.json())
+
+// third party middleware that will log a line to the console with request details 
+server.use(userRouter)
+
+server.listen(port, () => {
+    console.log(`server running at http:localhost:${port}`)
+})
